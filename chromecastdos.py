d41f50b0f0203eb76dc6621480c869839b169553
sqshr <mike@memery.info>
2025-12-10 15:46:48 +0000

    Create chromecastdos.py

---
+++ chromecastdos.py
@@ -0,0 +1,24 @@
+import time
+import pychromecast
+import zeroconf
+
+zconf = zeroconf.Zeroconf()
+browser = pychromecast.CastBrowser(pychromecast.SimpleCastListener(lambda uuid, service: print(browser.devices[uuid].friendly_name)), zconf)
+
+browser.start_discovery()
+chromecasts, browser = pychromecast.get_chromecasts()
+
+while True:
+	for c in chromecasts:
+		cast = c
+		print(f'  "Killing "{cast.name}" on mDNS/host service {cast.cast_info.services} with UUID:{cast.uuid}')
+		#print(c.device.friendly_name)
+		try:
+			cast = c
+			cast.wait()
+			cast.quit_app()
+		except:
+			pass
+	time.sleep(5)
+
+browser.stop_discovery()
